a0f37bdcdd0559c1367ac87e660cc9738671f55b
Lukman <muhamatuululukman@gmail.com>
2023-12-21 08:27:06 +0600

    feat: initial commit

---
+++ pkg/db/db.go
@@ -0,0 +1,56 @@
+package db
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type Handler func(ctx context.Context) error
+
+type Client interface {
+	DB() DB
+	Close() error
+}
+
+type Query struct {
+	Name     string
+	QueryRaw string
+}
+
+type NamedExecer interface {
+	ScanOneContext(ctx context.Context, dest interface{}, q Query, args ...interface{}) error
+	ScanAllContext(ctx context.Context, dest interface{}, q Query, args ...interface{}) error
+}
+
+// QueryExecer интерфейс для работы с обычными запросами
+type QueryExecer interface {
+	ExecContext(ctx context.Context, q Query, args ...interface{}) (pgconn.CommandTag, error)
+	QueryContext(ctx context.Context, q Query, args ...interface{}) (pgx.Rows, error)
+	QueryRowContext(ctx context.Context, q Query, args ...interface{}) pgx.Row
+}
+
+type Transactor interface {
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+}
+
+type TxManager interface {
+	ReadCommitted(ctx context.Context, f Handler) error
+}
+
+type SQLExecer interface {
+	NamedExecer
+	QueryExecer
+}
+
+type Pinger interface {
+	Ping(ctx context.Context) error
+}
+
+type DB interface {
+	SQLExecer
+	Transactor
+	Pinger
+	Close()
+}
---
+++ pkg/db/pg/client.go
@@ -0,0 +1,39 @@
+package pg
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/lukmandev/nameless/platform_libs/pkg/db"
+	"github.com/pkg/errors"
+)
+
+type pgClient struct {
+	masterDBC db.DB
+}
+
+func New(ctx context.Context, dsn string) (db.Client, error) {
+	dbc, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, errors.Errorf("failed to connect to db: %v", err)
+	}
+
+	fmt.Println("Successfully connected to database")
+
+	return &pgClient{
+		masterDBC: &pg{dbc: dbc},
+	}, nil
+}
+
+func (c *pgClient) DB() db.DB {
+	return c.masterDBC
+}
+
+func (c *pgClient) Close() error {
+	if c.masterDBC != nil {
+		c.masterDBC.Close()
+	}
+
+	return nil
+}

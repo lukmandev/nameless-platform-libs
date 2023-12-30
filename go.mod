a5d131f40a2acf2b8336de059e0a3be92679be1b
Lukman <muhamatuululukman@gmail.com>
2023-12-30 21:46:01 +0600

    feat: added toGRPCCode function

--- go.mod
+++ go.mod
@@ -6,14 +6,18 @@ require (
 	github.com/georgysavva/scany/v2 v2.0.0
 	github.com/jackc/pgx/v5 v5.0.0
 	github.com/pkg/errors v0.9.1
+	google.golang.org/grpc v1.60.1
 )
 
 require (
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.0.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- pkg/sys/validate/error.go
+++ pkg/sys/validate/error.go
@@ -3,7 +3,9 @@ package validate
 import (
 	"encoding/json"
 
+	"github.com/lukmandev/nameless-platform-libs/pkg/sys/codes"
 	"github.com/pkg/errors"
+	grpcCodes "google.golang.org/grpc/codes"
 )
 
 type ValidationErrors struct {
@@ -32,4 +34,47 @@ func (v *ValidationErrors) Error() string {
 func IsValidationError(err error) bool {
 	var ve *ValidationErrors
 	return errors.As(err, &ve)
+}
+
+func ToGRPCCode(code codes.Code) grpcCodes.Code {
+	var res grpcCodes.Code
+
+	switch code {
+	case codes.OK:
+		res = grpcCodes.OK
+	case codes.Canceled:
+		res = grpcCodes.Canceled
+	case codes.InvalidArgument:
+		res = grpcCodes.InvalidArgument
+	case codes.DeadlineExceeded:
+		res = grpcCodes.DeadlineExceeded
+	case codes.NotFound:
+		res = grpcCodes.NotFound
+	case codes.AlreadyExists:
+		res = grpcCodes.AlreadyExists
+	case codes.PermissionDenied:
+		res = grpcCodes.PermissionDenied
+	case codes.ResourceExhausted:
+		res = grpcCodes.ResourceExhausted
+	case codes.FailedPrecondition:
+		res = grpcCodes.FailedPrecondition
+	case codes.Aborted:
+		res = grpcCodes.Aborted
+	case codes.OutOfRange:
+		res = grpcCodes.OutOfRange
+	case codes.Unimplemented:
+		res = grpcCodes.Unimplemented
+	case codes.Internal:
+		res = grpcCodes.Internal
+	case codes.Unavailable:
+		res = grpcCodes.Unavailable
+	case codes.DataLoss:
+		res = grpcCodes.DataLoss
+	case codes.Unauthenticated:
+		res = grpcCodes.Unauthenticated
+	default:
+		res = grpcCodes.Unknown
+	}
+
+	return res
 }
